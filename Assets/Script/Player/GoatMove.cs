b412df8e89adaae1b11a326f5f8649b71dae374f
hongyeseong <eunpyohong44@gmail.com>
2026-05-07 18:11:50 +0900

    회전

--- Assets/Script/Player/GoatMove.cs
+++ Assets/Script/Player/GoatMove.cs
@@ -4,32 +4,37 @@ using UnityEngine.InputSystem;
 
 public class GoatMove : MonoBehaviour
 {
-    private Vector2 playerInput;
-    private Rigidbody2D playerRb;
-
-    [Header("Move")]
-    public float playerSpeed;
+    [Header("Move")] public float playerSpeed;
     public float playerJumpForce;
     public float playerMaxGravityForce;
 
-    [Header("Ground Check")]
-    public float playerJumpDistance;
+    [Header("Ground Check")] public float playerJumpDistance;
     private int groundLayer;
 
-    [Header("Fall")]
-    public float fallThreshold;
+    [Header("Fall")] public float fallThreshold;
     public GameObject spawnPoint;
 
-    [Header("Crazy Effect")]
-    public float goatCrageTime;
+    [Header("Crazy Effect")] public float goatCrageTime;
     public float goatCragePosition = 1.5f;
     public float goatCrageRotation = 0.1f;
 
+    [Header("Goat Animation")] public Animator animator;
+
     private bool isRespawning = false;
+
+    private Vector2 playerInput;
+    private Rigidbody2D playerRb;
+
+    private Vector2 moveForce;
+
+    private Vector2 lastLookDirection;
+
+    private bool isTurn;
 
     void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        lastLookDirection = new Vector2(1, 0);
     }
 
     void Start()
@@ -52,9 +57,18 @@ public class GoatMove : MonoBehaviour
 
     void Move()
     {
-        if (IsGrounded())
+        if (IsGrounded() && !isTurn)
         {
-            Vector2 moveForce = new Vector2(playerInput.x * playerSpeed, 0);
+            moveForce = new Vector2(playerInput.x * playerSpeed, 0);
+            animator.SetBool("Jump", false);
+            if (Mathf.Abs(playerRb.linearVelocity.x) > 0.1f)
+            {
+                animator.SetFloat("Forward", Mathf.Abs(3));
+            }
+            else
+            {
+                animator.SetFloat("Forward", 0);
+            }
 
             playerRb.AddForce(moveForce);
         }
@@ -71,6 +85,17 @@ public class GoatMove : MonoBehaviour
     public void OnMove(InputValue value)
     {
         playerInput = value.Get<Vector2>();
+
+        if (playerInput.x > 0 && lastLookDirection.x < 0 && !isTurn)
+        {
+            StartCoroutine(IsTurn(1));
+        }
+        else if (playerInput.x < 0 && lastLookDirection.x > 0 && !isTurn)
+        {
+            StartCoroutine(IsTurn(-1));
+        }
+
+        lastLookDirection = playerInput.normalized;
     }
 
     public void OnJump(InputValue value)
@@ -78,6 +103,7 @@ public class GoatMove : MonoBehaviour
         if (value.isPressed && IsGrounded())
         {
             playerRb.AddForce(Vector2.up * playerJumpForce, ForceMode2D.Impulse);
+            animator.SetBool("Jump", true);
         }
     }
 
@@ -90,15 +116,19 @@ public class GoatMove : MonoBehaviour
             groundLayer
         );
 
-        return hit.collider != null;
+        bool grounded = hit.collider != null;
+
+        animator.SetBool("IsGrounded", grounded);
+
+        return grounded;
     }
 
     IEnumerator RespawnRoutine()
     {
-        
+
         transform.position = spawnPoint.transform.position;
         playerRb.linearVelocity = playerRb.linearVelocity.normalized;
-        
+
         yield return new WaitUntil(() => IsGrounded());
 
         isRespawning = true;
@@ -109,18 +139,46 @@ public class GoatMove : MonoBehaviour
 
         while (t < goatCrageTime)
         {
-            transform.position = new Vector3(Random.Range(transform.position.x - goatCragePosition, transform.position.x + goatCragePosition), transform.position.y, transform.position.z);
+            transform.position =
+                new Vector3(
+                    Random.Range(transform.position.x - goatCragePosition, transform.position.x + goatCragePosition),
+                    transform.position.y, transform.position.z);
 
-            transform.rotation = Quaternion.Euler(Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100), Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100), Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100));
+            transform.rotation = Quaternion.Euler(Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100),
+                Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100),
+                Random.Range(-goatCrageRotation * 100, goatCrageRotation * 100));
 
             t += 0.05f;
             yield return new WaitForSeconds(0.05f);
         }
-        
+
         playerRb.linearVelocity = Vector2.zero;
 
         transform.rotation = Quaternion.identity;
-        
+
         isRespawning = false;
+    }
+
+    IEnumerator IsTurn(int dir)
+    {
+        isTurn = true;
+
+        animator.SetFloat("Turn", dir);
+        while (isTurn)
+        {
+            float y = transform.rotation.eulerAngles.y;
+
+            if (dir == 1 && y >= 90)
+            {
+                animator.SetFloat("Turn", 0);
+                isTurn = false;
+            }
+            else if (dir == -1 && y >= 270)
+            {
+                animator.SetFloat("Turn", 0);
+                isTurn = false;
+            }
+            yield return null;
+        }
     }
 }
